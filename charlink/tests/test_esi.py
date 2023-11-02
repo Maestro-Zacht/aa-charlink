1dab4b10c49362041ed291cbc64062d9f1f305e1
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2023-11-02 09:34:59 +0100

    random scopes tests

---
+++ charlink/tests/test_esi.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+
+from esi.models import Scope, Token
+
+from app_utils.testdata_factories import UserFactory, EveCharacterFactory
+from app_utils.testing import add_character_to_user, add_new_token
+
+
+class TestScope(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserFactory()
+        cls.character = EveCharacterFactory()
+
+    def test_token(self):
+        add_character_to_user(self.user, self.character, scopes=['esi-fleets.read_fleet.v1'])
+        token = Token.objects.get(character=self.character)
+        self.assertEqual(token.scopes.count(), 1)
+        self.assertEqual(len(token.scopes), 1)
