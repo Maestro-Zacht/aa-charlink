d79146765efdacd33dd376e0240ddbd5ff55f89c
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2023-11-02 10:54:30 +0100

    more tests in esi

--- charlink/tests/test_esi.py
+++ charlink/tests/test_esi.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 
-from esi.models import Scope, Token
+from esi.models import Token
+from esi.managers import _process_scopes
 
 from app_utils.testdata_factories import UserFactory, EveCharacterFactory
-from app_utils.testing import add_character_to_user, add_new_token
+from app_utils.testing import add_character_to_user
 
 
 class TestScope(TestCase):
@@ -20,6 +21,9 @@ class TestScope(TestCase):
         self.assertEqual(len(token.scopes.values_list('pk', flat=True)), 1)
 
     def test_required_scopes(self):
-        add_character_to_user(self.user, self.character, scopes=["esi-location.read_location.v1", "esi-location.read_ship_type.v1", "esi-location.read_online.v1"])
-        tokens = Token.objects.all().require_scopes(["esi-location.read_location.v1", "esi-location.read_ship_type.v1"])
-        self.assertEqual(len(tokens), 1)
+        scopes = ["esi-location.read_location.v1", "esi-location.read_ship_type.v1", "esi-location.read_online.v1"]
+        add_character_to_user(self.user, self.character, scopes=scopes)
+        processed_scopes = _process_scopes(scopes)
+        from esi.models import Scope
+        scope_pks = Scope.objects.filter(name__in=scopes).values_list('pk', flat=True)
+        self.assertEqual(len(processed_scopes), len(scope_pks))
