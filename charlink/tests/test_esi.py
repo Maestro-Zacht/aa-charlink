036277072d7ce3d9fe149cfe439bb9f3c302c16f
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2023-11-02 10:37:55 +0100

    fix coverage file + more esi tests

--- charlink/tests/test_esi.py
+++ charlink/tests/test_esi.py
@@ -18,3 +18,8 @@ class TestScope(TestCase):
         token = Token.objects.get(character_id=self.character.character_id)
         self.assertEqual(token.scopes.count(), 1)
         self.assertEqual(len(token.scopes.values_list('pk', flat=True)), 1)
+
+    def test_required_scopes(self):
+        add_character_to_user(self.user, self.character, scopes=["esi-location.read_location.v1", "esi-location.read_ship_type.v1", "esi-location.read_online.v1"])
+        tokens = Token.objects.all().require_scopes(["esi-location.read_location.v1", "esi-location.read_ship_type.v1"])
+        self.assertEqual(len(tokens), 1)
